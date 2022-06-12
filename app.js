142adcf6f300f5fac856bf0dddef6b53991e5164
032-Abhishek Choksi <19bmiit032@gmail.com>
2022-06-12 11:12:54 +0530

    Node JS Upload File With Validations

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const multer = require('multer');
+var path = require('path');
+
+const app = express();
+
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, "uploads")
+        },
+        filename: function (req, file, cb) {
+            cb(null, file.fieldname + "-" + Date.now() + ".jpg")
+        }
+    }),
+    limits: {
+        //fields: 5,
+        fieldNameSize: 50, // TODO: Check if this size is enough
+        fieldSize: 20000, //TODO: Check if this size is enough
+        // TODO: Change this line after compression
+        fileSize: 15000000, // 150 KB for a 1080x1080 JPG 90
+    },
+    fileFilter: function (_req, file, cb) {
+        checkFileType(file, cb);
+    }
+}).single("user_file");
+
+function checkFileType(file, cb) {
+    // Allowed ext
+    const filetypes = /jpeg|jpg|png|gif/;
+    // Check ext
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    // Check mime
+    const mimetype = filetypes.test(file.mimetype);
+
+    if (mimetype && extname) {
+        return cb(null, true);
+    } else {
+        cb('Error: Images Only!');
+    }
+}
+
+app.post("/upload", upload, (req, resp) => {
+    resp.send("File Uplaod");
+});
+
+app.listen(5000);
